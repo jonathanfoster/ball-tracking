b33f49dcb74b60553347a93e9348a6cd6cb54100
Jonathan Foster <jonathan.e.foster@gmail.com>
2016-11-05 13:24:21 -0400

    Remove previous position contrail

--- ball_tracking.py
+++ ball_tracking.py
@@ -6,12 +6,10 @@ import cv2
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video", help="path to the (optional) video file")
-ap.add_argument("-b", "--buffer", type=int, default=64, help="max buffer size")
 args = vars(ap.parse_args())
 
 greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
-pts = deque(maxlen=args["buffer"])
 
 if not args.get("video", False):
     camera = cv2.VideoCapture(0)
@@ -37,21 +35,9 @@ while True:
     if len(cnts) > 0:
         c = max(cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
 
         if radius > 10:
             cv2.circle(frame, (int(x), int(y)), int(radius), (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-    pts.appendleft(center)
-
-    for i in xrange(1, len(pts)):
-        if pts[i - 1] is None or pts[i] is None:
-            continue
-
-        thickness = int(np.sqrt(args["buffer"] / float(i + 1)) * 2.5)
-        cv2.line(frame, pts[i -1], pts[i], (0, 0, 255), thickness)
 
     cv2.imshow("Frame", frame)
     key = cv2.waitKey(1) & 0xFF
