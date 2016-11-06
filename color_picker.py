0f6865b1c6350ae37e92b0889db3367efd55b885
Jonathan Foster <jonathan.e.foster@gmail.com>
2016-11-06 12:49:38 -0500

    Remove unnecessary main function

--- color_picker.py
+++ color_picker.py
@@ -1,10 +1,16 @@
 import argparse
-import ast
 import cv2
 import imutils
 import numpy as np
 
 colors = []
+lower = np.array([])
+upper = np.array([])
+mask = np.array([])
+
+def on_mouse_click (event, x, y, flags, image):
+    if event == cv2.EVENT_LBUTTONUP:
+        colors.append(image[y,x].tolist())
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", required=True, help="path to the image file")
@@ -12,53 +18,41 @@ parser.add_argument("-l", "--lower", help="HSV lower bounds")
 parser.add_argument("-u", "--upper", help="HSV upper bounds")
 args = vars(parser.parse_args())
 
-def on_mouse_click (event, x, y, flags, image):
-    if event == cv2.EVENT_LBUTTONUP:
-        colors.append(image[y,x].tolist())
+if args["lower"] and args["upper"]:
+  lower = np.fromstring(args["lower"], sep=",")
+  upper = np.fromstring(args["upper"], sep=",")
 
-def main():
-    lower = np.array([])
-    upper = np.array([])
-    mask = np.array([])
+image = cv2.imread(args["image"])
+image = imutils.resize(image, width=600)
 
-    if args["lower"] and args["upper"]:
-      lower = np.fromstring(args["lower"], sep=",")
-      upper = np.fromstring(args["upper"], sep=",")
+hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-    image = cv2.imread(args["image"])
-    image = imutils.resize(image, width=600)
+if lower.any() and upper.any():
+  mask = cv2.inRange(hsv, lower, upper)
+  mask = cv2.erode(mask, None, iterations=2)
+  mask = cv2.dilate(mask, None, iterations=2)
 
-    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+while True:
+    cv2.imshow("image", hsv)
+    cv2.setMouseCallback("image", on_mouse_click, hsv)
 
-    if lower.any() and upper.any():
-      mask = cv2.inRange(hsv, lower, upper)
-      mask = cv2.erode(mask, None, iterations=2)
-      mask = cv2.dilate(mask, None, iterations=2)
+    if mask.any():
+      cv2.imshow("mask", mask)
 
-    while True:
-        cv2.imshow("image", hsv)
-        cv2.setMouseCallback("image", on_mouse_click, hsv)
+    if cv2.waitKey(1) & 0xFF == ord("q"):
+        break
 
-        if mask.any():
-          cv2.imshow("mask", mask)
+cv2.destroyAllWindows()
 
-        if cv2.waitKey(1) & 0xFF == ord("q"):
-            break
+if not colors:
+  exit
 
-    cv2.destroyAllWindows()
+minh = min(c[0] for c in colors)
+mins = min(c[1] for c in colors)
+minv = min(c[2] for c in colors)
 
-    if not colors:
-      return
+maxh = max(c[0] for c in colors)
+maxs = max(c[1] for c in colors)
+maxv = max(c[2] for c in colors)
 
-    minh = min(c[0] for c in colors)
-    mins = min(c[1] for c in colors)
-    minv = min(c[2] for c in colors)
-
-    maxh = max(c[0] for c in colors)
-    maxs = max(c[1] for c in colors)
-    maxv = max(c[2] for c in colors)
-
-    print [minh, mins, minv], [maxh, maxs, maxv]
-
-if __name__ == "__main__":
-    main()
+print [minh, mins, minv], [maxh, maxs, maxv]
